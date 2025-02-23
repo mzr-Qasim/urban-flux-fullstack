54a41acbece4cedc1640535fecfa008cda85ac1f
Muhammad Qasim <malikqasim20051@gmail.com>
2025-02-23 18:23:35 +0500

    mysql Database integrated

--- urbax_Flux/settings.py
+++ urbax_Flux/settings.py
@@ -81,10 +81,22 @@ WSGI_APPLICATION = 'urbax_Flux.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'urbanflux',
+        'DATABASE': 'urbanflux',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost', 
+        'PORT': '3306',
     }
+
+
 }
 
 
